64cf301469ab2a40dd1314082e7250c866af5836
m2000h <mashakura@edu.hse.ru>
2019-04-17 19:35:46 +0300

    Name and App Logo added

--- App5/App5.Android/MainActivity.cs
+++ App5/App5.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 namespace App5.Droid
 {
     
-    [Activity(Label = "App5", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "События", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
